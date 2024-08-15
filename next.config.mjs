096d4739301cf7d2df0463810a9e8abfd47ce3f6
Al Miller <al.norman.miller@gmail.com>
2024-08-14 17:37:22 -0700

    path (#6)

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   // This app is deployed under a single Vercel project at https://anmiller.com using NextJS multi-zones.
   basePath: "/oregon",
+  assetPrefix: "/oregon",
 };
 
 export default nextConfig;
