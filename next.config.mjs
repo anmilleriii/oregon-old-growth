ac70c19b9c0e366a014f4d9ea9dc3436ab08545d
Al Miller <al.norman.miller@gmail.com>
2024-08-14 22:36:46 -0700

    test

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,6 @@
 const nextConfig = {
   // This app is deployed under a single Vercel project at https://anmiller.com using NextJS multi-zones.
   basePath: "/oregon",
-  images: {
-    remotePatterns: [
-      {
-        hostname: "oregon.anmiller.com",
-        protocol: "https",
-      },
-    ],
-  },
   async redirects() {
     return [
       {
